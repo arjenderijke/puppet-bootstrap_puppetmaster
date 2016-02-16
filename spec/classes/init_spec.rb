fd61948e9aefdd9e6ee235a9448ce24050655aad
Arjen de Rijke <arjenderijke@gmail.com>
2016-02-16 08:43:21 +0100

    add missing gems and change old module name

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-describe 'provision_puppetmaster' do
+describe 'bootstrap_puppetmaster' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('provision_puppetmaster') }
+    it { should contain_class('bootstrap_puppetmaster') }
   end
 end
