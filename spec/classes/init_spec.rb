6147bb9eaa62c83e89f50d73b66fbb18c9759d6c
Arjen de Rijke <arjenderijke@gmail.com>
2016-02-16 12:53:03 +0100

    add workaround for incomple version.pp of apache module

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 describe 'bootstrap_puppetmaster' do
 
   context 'with defaults for all parameters' do
+
+    let(:facts) do
+      {
+        :osfamily               => 'Debian',
+        :operatingsystem        => 'Ubuntu',
+        :operatingsystemrelease => '12.04'
+      }
+      
     it { should contain_class('bootstrap_puppetmaster') }
   end
 end
