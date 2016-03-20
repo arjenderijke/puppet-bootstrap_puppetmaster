3a5e21480a32874c3a1c738039746a49e52ef284
Arjen de Rijke <arjenderijke@gmail.com>
2016-03-20 17:42:43 +0100

    add spec tests

--- spec/classes/puppetmaster_spec.rb
+++ spec/classes/puppetmaster_spec.rb
@@ -16,6 +16,18 @@ describe 'bootstrap_puppetmaster::puppetmaster' do
         should contain_class('bootstrap_puppetmaster::puppetmaster')
       }
     end
+
+    it 'should contain class apache' do
+      expect {
+        should contain_class('apache')
+      }
+    end
+
+    it 'should contain class apache::mod::passenger' do
+      expect {
+        should contain_class('apache::mod::passenger')
+      }
+    end
   end
 
   context 'with defaults for all parameters on Fedora 22' do
@@ -31,6 +43,18 @@ describe 'bootstrap_puppetmaster::puppetmaster' do
     it 'should contain class bootstrap_puppetmaster::puppetmaster' do
       expect {
         should contain_class('bootstrap_puppetmaster::puppetmaster')
+      }
+    end
+
+    it 'should contain class apache' do
+      expect {
+        should contain_class('apache')
+      }
+    end
+
+    it 'should contain class apache::mod::passenger' do
+      expect {
+        should contain_class('apache::mod::passenger')
       }
     end
   end
